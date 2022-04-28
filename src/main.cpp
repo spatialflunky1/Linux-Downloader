d0eb63fae78bb4b22867c52df9bc12c4d4407356
Spatialflunky1 <mjandrus06@gmail.com>
2022-04-28 11:12:15 -0400

    changed wget system call to curl library

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "httplib.h"
 #include <ncurses.h>
 #include <iostream>
+#include <curl/curl.h>
 
 // Global Vars
 int key; // Key id
@@ -81,12 +82,37 @@ std::vector<std::string> get_data(std::string distro, std::string request,std::s
     return versions;
 }
 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
+    size_t written = fwrite(ptr,size,nmemb, (FILE *)stream);
+    return written;
+}
+
 void download_file(std::string downUrl, std::string filename) {
+    // vars
+    CURL *easyhandle;
+    static const char *pagefilename = "page.out";
+    FILE *pagefile;
+    // Curl setup
+    curl_global_init(CURL_GLOBAL_ALL);
+    easyhandle = curl_easy_init();
+    // curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1);
+    curl_easy_setopt(easyhandle, CURLOPT_URL, downUrl.c_str());
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(easyhandle, CURLOPT_NOPROGRESS, 0);
+    // Get path
     std::string path;
     std::cout << "Enter path to download to: ";
     std::getline(std::cin, path);
-    std::string command = "wget "+downUrl+" -O "+path+"/"+filename; 
-    std::system(command.c_str());
+    // Download file
+    pagefile = fopen(pagefilename, "wb");
+    if (pagefile) {
+        curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, pagefile);
+        curl_easy_perform(easyhandle);
+        fclose(pagefile);
+    }
+    // Cleanup
+    curl_easy_cleanup(easyhandle);
+    curl_global_cleanup();
 }
 
 int main() {
