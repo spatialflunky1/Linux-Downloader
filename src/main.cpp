ae6db99fc8a7504fbe931134fc633987c56c3bf7
Spatialflunky1 <mjandrus06@gmail.com>
2022-05-24 10:14:36 -0400

    fixed kernel downloading

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@ void update_selection(WINDOW * mainWindow) {
             }
             if (current==1 && distro.compare("")==0) distro=menu.at(current_selection+1); 
             if (current==3 && distro.compare("Linux Kernel")!=0) running=false;
-            else current_selection=0;
+            else if (distro.compare("Linux Kernel")!=0) current_selection=0;
             clear();
             break;
         case 113:
@@ -62,25 +62,46 @@ void update_selection(WINDOW * mainWindow) {
     }
 }
 
-void dialog(std::vector<std::string> menu, int length, int title_length) {
+void dialog(std::vector<std::string> menu,int length,int title_length,int height,int width) {
+    int line=1;
+    int column=0;
     attron(COLOR_PAIR(3));
     mvprintw(0,0,menu.at(0).c_str());
     if (title_length==2) mvprintw(1,0,("Index of "+menu.at(1)).c_str());
     for (int i=title_length; i<length; i++) {
-        if (current_selection+title_length==i) attron(COLOR_PAIR(2));
+        if (current_selection+title_length==i) {
+            attron(COLOR_PAIR(1));
+            for (int j=menu.at(0).size(); j<width; j++) mvprintw(0, j, (char*)" ");
+            attron(COLOR_PAIR(2));
+            mvprintw(0, width-menu.at(i).size()-1, menu.at(i).c_str());
+        }
         else attron(COLOR_PAIR(1));
-        mvprintw(i+title_length,0,menu.at(i).c_str());
+        if (line+title_length+1==height) {
+            line=1;
+            column+=19;
+        }
+        if (menu.at(i).size()+column < width) {
+            if (menu.at(i).size() > 15) {
+                std::string message = menu.at(i).substr(0,15)+="...";
+                mvprintw(line+title_length,column,message.c_str());
+            }
+            else mvprintw(line+title_length,column,menu.at(i).c_str());
+        }
+        line++;
     }
     refresh();
 }
 
 int main() {
     std::vector<std::string> links;
+    int height;
+    int width;
     WINDOW * mainWindow;
     if ((mainWindow = initscr()) == NULL) {
         std::cout << "Failed to start ncurses" << std::endl;
         exit(1);
     } // start curses mode or exit on error
+    getmaxyx(mainWindow, height, width);
     start_color();
     use_default_colors();
     init_pair(1,COLOR_WHITE,-1); // 1: No highlight
@@ -97,7 +118,7 @@ int main() {
                 bottom=menu.size()-1;
                 need_update=false;
             }
-            dialog(menu,menu.size(),1); 
+            dialog(menu,menu.size(),1,height,width); 
         }
         // Every distro besides Linux Kernel
         if (distro.compare("Linux Kernel")!=0) {            
@@ -123,7 +144,7 @@ int main() {
                 clear();
                 refresh();
             }
-            dialog(menu,menu.size(),1);
+            dialog(menu,menu.size(),1,height,width);
         }
         // For the linux kernel
         // Basically a file server browser
@@ -135,13 +156,13 @@ int main() {
                 bottom=menu.size()-2;
                 need_update=false;
             }
-            dialog(menu,menu.size(),2);
+            dialog(menu,menu.size(),2,height,width);
         }
         update_selection(mainWindow);
         refresh();
     }
     cleanup(mainWindow);
     if (distro.compare("Ubuntu")==0) download_file(links.at(current_selection), menu.at(current_selection+1));
-    if (distro.compare("Linux Kernel")==0) download_file("https://mirrors.edge.kernel.org"+current_url, menu.at(current_selection));
+    if (distro.compare("Linux Kernel")==0) download_file("https://mirrors.edge.kernel.org"+current_url, menu.at(current_selection+2));
     return 0;
 }
